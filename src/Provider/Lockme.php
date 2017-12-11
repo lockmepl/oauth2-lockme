e2dc66b06bbc18b4869fcbe62f222b754f327927
Jakub Caban <kuba@whyblack.pl>
2017-12-11 13:41:17 +0100

    Let PHP5'

--- src/Provider/Lockme.php
+++ src/Provider/Lockme.php
@@ -1,7 +1,6 @@
 <?php
 namespace Lockme\OAuth2\Client\Provider;
 use League\OAuth2\Client\Provider\AbstractProvider;
-use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Lockme\OAuth2\Client\Provider\Exception\LockmeIdentityProviderException;
@@ -30,15 +29,15 @@ class Lockme extends AbstractProvider{
     parent::__construct($options);
   }
 
-  public function getBaseAuthorizationUrl(): string{
+  public function getBaseAuthorizationUrl(){
     return $this->apiDomain."/authorize";
   }
 
-  public function getBaseAccessTokenUrl(array $params): string{
+  public function getBaseAccessTokenUrl(array $params){
     return $this->apiDomain."/access_token";
   }
 
-  public function getResourceOwnerDetailsUrl(AccessToken $token): string{
+  public function getResourceOwnerDetailsUrl(AccessToken $token){
     return $this->apiDomain.'/'.$this->version.'/me';
   }
 
@@ -46,7 +45,7 @@ class Lockme extends AbstractProvider{
     return [];
   }
 
-  protected function checkResponse(ResponseInterface $response, $data): void{
+  protected function checkResponse(ResponseInterface $response, $data){
     if ($response->getStatusCode() >= 400) {
       throw LockmeIdentityProviderException::clientException($response, $data);
     } elseif (isset($data['error'])) {
@@ -54,7 +53,7 @@ class Lockme extends AbstractProvider{
     }
   }
 
-  protected function createResourceOwner(array $response, AccessToken $token): ResourceOwnerInterface{
+  protected function createResourceOwner(array $response, AccessToken $token){
     return new LockmeUser($response);
   }
 
